1e20fac92da6777d414e3131a5aea90385a2370d
Son Changwoo <kor45cw@gmail.com>
2019-05-31 17:50:39 +0900

    UI: Add kakao & share icon

--- Moop/Extensions/UIView+Extension.swift
+++ Moop/Extensions/UIView+Extension.swift
@@ -38,4 +38,12 @@ extension UIView {
             layer.borderColor = newValue?.cgColor
         }
     }
+    
+    func elevate(elevation: Double) {
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: elevation)
+        self.layer.shadowRadius = CGFloat(elevation)
+        self.layer.shadowOpacity = 0.24
+    }
 }
