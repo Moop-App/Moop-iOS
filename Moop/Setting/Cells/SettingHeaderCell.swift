3ad03e7826ae30c49f8f0dc1b872087d76eff951
kor45cw <kor45cw@gmail.com>
2019-08-07 18:09:14 +0900

    Fix: divider style change to none

--- Moop/Setting/Cells/SettingHeaderCell.swift
+++ Moop/Setting/Cells/SettingHeaderCell.swift
@@ -14,6 +14,6 @@ class SettingHeaderCell: UITableViewCell, ReusableView, NibLoadableView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        self.selectionStyle = .none
     }
 }
