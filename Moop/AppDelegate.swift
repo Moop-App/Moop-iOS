8468c135c44bfdcb0e85e26c40bb24bd682aeaa9
kor45cw <kor45cw@gmail.com>
2020-04-11 14:05:32 +0900

    광고 미디에이션 추가

--- Moop/AppDelegate.swift
+++ Moop/AppDelegate.swift
@@ -11,6 +11,7 @@ import Firebase
 import Networking
 import SwiftyStoreKit
 import FBSDKCoreKit
+import TenpingSDKFramework
 #if DEBUG
 import FLEX
 #endif
@@ -37,6 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     /// - Tag: DidBecomeActive
     func applicationDidBecomeActive(_ application: UIApplication) {
         AppEvents.activateApp()
+        TenpingSDK.shareInstance()?.becomeActive()
         ShortcutManager.shared.applicationDidBecomeActive(rootViewController: window?.rootViewController)
     }
     
@@ -49,7 +51,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 extension AppDelegate {
     private func librarySetup() {
         FirebaseApp.configure()
-        Fabric.with([Crashlytics.self])
         AdManager.librarySetup()
         APISetupManager.setup()
         GADMobileAds.sharedInstance().start(completionHandler: nil)
@@ -64,6 +65,7 @@ extension AppDelegate {
                         SwiftyStoreKit.finishTransaction(purchase.transaction)
                     }
                     // Unlock content
+                    UserData.isAdFree = true
                 case .failed, .purchasing, .deferred:
                     break // do nothing
                 @unknown default:
@@ -75,7 +77,7 @@ extension AppDelegate {
     
     private func debugSettings() {
         #if DEBUG
-        FLEXManager.shared().showExplorer()
+        FLEXManager.shared.showExplorer()
         #endif
     }
 }
---
+++ Moop/CaulyLib/CaulySDK-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  CaulySDK-Bridging-Header.h
+//  SeasonTicket45
+//
+//  Created by kor45cw on 2020/03/11.
+//  Copyright © 2020 kor45cw. All rights reserved.
+//
+
+#import "include/Cauly.h"
+#import "include/CaulyAdSetting.h"
+#import "include/CaulyAdView.h"
+#import "include/CaulyInterstitialAd.h"
+#import "include/CaulyNativeAd.h"
+#import "include/CaulyNativeAdItem.h"
